a798be41e85aeb2a948ec19bd28858b99cc465cb
shrutiiii03 <145834046+shrutiiii03@users.noreply.github.com>
2024-07-22 22:59:40 +0530

    Update settings.py

--- scrapper/scrapper/settings.py
+++ scrapper/scrapper/settings.py
@@ -115,11 +115,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Celery Configuration
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
+
 
